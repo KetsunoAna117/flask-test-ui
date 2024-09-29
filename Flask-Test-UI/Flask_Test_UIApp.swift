dee75e5dcf06b60b4efe1cb68f1e375567642f18
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-09-29 16:20:00 +0700

    Initial Commit

---
+++ Flask-Test-UI/Flask_Test_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flask_Test_UIApp.swift
+//  Flask-Test-UI
+//
+//  Created by Hans Arthur Cupiterson on 29/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Flask_Test_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Flask-Test-UITests/Flask_Test_UITests.swift
@@ -0,0 +1,17 @@
+//
+//  Flask_Test_UITests.swift
+//  Flask-Test-UITests
+//
+//  Created by Hans Arthur Cupiterson on 29/09/24.
+//
+
+import Testing
+@testable import Flask_Test_UI
+
+struct Flask_Test_UITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
